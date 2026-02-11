072bf21c87361561bdc79f6f1033cfccd049d889
Eric Kim <ksy77kr@gmail.com>
2026-02-11 13:26:52 +0900

    Add capture script for taking screenshots and saving as PNG

---
+++ capture.ps1
@@ -0,0 +1,7 @@
+﻿Add-Type -AssemblyName System.Windows.Forms,System.Drawing
+$bmp = New-Object System.Drawing.Bitmap([System.Windows.Forms.Screen]::PrimaryScreen.Bounds.Width, [System.Windows.Forms.Screen]::PrimaryScreen.Bounds.Height)
+$g = [System.Drawing.Graphics]::FromImage($bmp)
+$g.CopyFromScreen(0,0,0,0,$bmp.Size)
+$out = 'D:\GitHub\Test\openclaw_screenshot.png'
+$bmp.Save($out,[System.Drawing.Imaging.ImageFormat]::Png)
+Write-Output "SCREENSHOT_SAVED:$out"
